87503050291b22dde38e001acb62f6ff14895312
alex <agragregra@ya.ru>
2025-03-16 17:19:27 +0300

    Update

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Docker PHP MySQL phpMyAdmin
+
+```
+git clone https://github.com/agragregra/dpmp .; rm -rf trunk readme.md .git
+```
+
+## Features
+
+- **PHP**: Latest PHP version.
+- **MySQL**: latest MySQL version.
+- **DB**: MySQLi & PDO.
+- **phpMyAdmin**: latest version.
--- www/index.php
+++ www/index.php
@@ -1,10 +1,20 @@
 <?php
 
+echo "<hr><h3>MySQLi</h3>";
+
 $conn = new mysqli("db", "root", "root", "example");
 $result = $conn->query("SELECT id, name, email FROM users");
 
 while($row = $result->fetch_assoc()) {
-  echo "<div>{$row['id']} <strong>{$row['name']}</strong> {$row['email']}</div>";
-}
+  echo "<div>{$row['id']} <strong>{$row['name']}</strong> {$row['email']}</div><br>";
+} $conn->close();
 
-$conn->close();
+echo "<hr><h3>PDO</h3>";
+
+$pdo = new PDO("mysql:host=db;dbname=example", "root", "root");
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$stmt = $pdo->query("SELECT id, name, email FROM users");
+
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+  echo "<div>{$row['id']} <strong>{$row['name']}</strong> {$row['email']}</div><br>";
+}
